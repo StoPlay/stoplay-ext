4b07283f8b0af2edbca975f143a3d8a7154f880d
Alex Buznik <a@beshur.net>
2015-09-09 14:39:42 +0300

    Build system

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+// Task configurations
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('manifest.json'),
+		bumpup: {
+			options: {
+				updateProps: {
+					pkg: 'manifest.json'
+				}
+			},
+			files: ['manifest.json', 'package.json']
+		},
+		tagrelease: '<%= pkg.version %>',
+		zip: {
+			'long-format': {
+				src: ['css/**', 'img/**', '*.js*', '*.css', '*.md', 'LICENSE'],
+				dest: 'builds/<%= pkg.name + "-" + pkg.version %>.zip'
+			}
+		}
+
+	});
+
+	// Loading the plugins
+	grunt.loadNpmTasks('grunt-bumpup');
+	grunt.loadNpmTasks('grunt-tagrelease');
+	grunt.loadNpmTasks('grunt-zip');
+
+
+	// Alias task for release
+	grunt.registerTask('makeRelease', function (type) {
+		type = type ? type : 'patch';     // Default release type
+		grunt.task.run('bumpup:' + type); // Bump up the version
+		grunt.task.run('tagrelease');     // Commit & tag the release
+		grunt.task.run('zip');     // Compress an archive
+	});
+
+	grunt.registerTask('default', ['makeRelease']);
+	grunt.registerTask('pack', ['zip']);
+}
