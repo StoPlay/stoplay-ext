81f64dd9c2693d2546ee5573addc3cd107369022
Alex Buznik <shu@buznik.net>
2018-09-21 23:04:32 +0300

    Release 1.1.24

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,15 +16,35 @@ module.exports = function(grunt) {
                 src: ['css/**', 'img/**', '*.js*', '*.css', '*.md', '*.html', 'LICENSE', '!Gruntfile.js', '!package.json'],
                 dest: 'builds/<%= pkg.name + "-" + pkg.version %>.zip'
             }
+        },
+        webstore_upload: {
+            "accounts": {
+                "default": { //account under this section will be used by default
+                    publish: false, //publish item right after uploading. default false
+                    client_id: process.env.ChromeAPI_clientId,
+                    client_secret: process.env.ChromeAPI_clientSecret,
+                    refresh_token: process.env.ChromeAPI_refreshToken
+                },
+            },
+            "extensions": {
+                "extension1": {
+                    //required
+                    appID: process.env.extensionId,
+                    //required, we can use dir name and upload most recent zip file
+                    zip: 'builds/<%= pkg.name + "-" + pkg.version %>.zip'      
+                }
+            },
+            onComplete: function(result) {
+                console.log('webstore_upload result', result);
+            }
         }
-
     });
 
     // Loading the plugins
     grunt.loadNpmTasks('grunt-bumpup');
     grunt.loadNpmTasks('grunt-tagrelease');
     grunt.loadNpmTasks('grunt-zip');
-
+    grunt.loadNpmTasks('grunt-webstore-upload');
 
     // Alias task for release
     grunt.registerTask('makeRelease', function (type) {
@@ -37,4 +57,5 @@ module.exports = function(grunt) {
     grunt.registerTask('default', []);
     grunt.registerTask('build', ['makeRelease']);
     grunt.registerTask('pack', ['zip']);
+    grunt.registerTask('deploy', ['build', 'webstore_upload']);
 }
