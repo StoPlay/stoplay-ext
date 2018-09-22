7d1e1176355bf25b853d84bd7e3dce4e11745ecd
Travis CI <travis@travis-ci.org>
2018-09-22 20:06:03 +0300

    Updates for Travis git push, #98

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,4 +5,5 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 # commit should be already created by grunt bumpup job
-git push
+# git remote add origin-stoplay https://${GH_TOKEN}@github.com/stoplay/stoplay-ext.git > /dev/null 2>&1
+git push https://${GH_TOKEN}@github.com/stoplay/stoplay-ext.git
