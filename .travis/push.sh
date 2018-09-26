1dae665715899e42570c43a5b50ed440d909f511
Alex Buznik <shu@buznik.net>
2018-09-26 15:41:59 +0300

    Release 1.1.48

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,6 +5,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 # need to do it, bc Travis checks out a commit by hash
+git fetch
 git checkout master
 git merge ${TRAVIS_BRANCH}
 git push https://${GH_TOKEN}@github.com/stoplay/stoplay-ext.git
