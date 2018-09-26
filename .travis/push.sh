ec2bfbd48c60476a94ed0fc8d457aa4cac4c25e6
Alex Buznik <shu@buznik.net>
2018-09-26 16:33:11 +0300

    Update CI to merge back to develop

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Merging ${TRAVIS_BRANCH} to master"
+echo "Merging ${TRAVIS_BRANCH} to develop"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
 # need to do it, bc Travis checks out a commit by hash
-git fetch && git checkout master && git merge ${TRAVIS_BRANCH} --no-edit
+git fetch && git checkout develop && git merge ${TRAVIS_BRANCH} --no-edit
 git push https://${GH_TOKEN}@github.com/stoplay/stoplay-ext.git
